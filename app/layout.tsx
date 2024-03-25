9bfa010c12a5a991b7e56754710060521b8d0aff
Hassan Motiei <123471966+HassanMotiei@users.noreply.github.com>
2024-03-26 02:30:56 +0330

    Add Auth and Create Start Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -10,13 +12,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={font.className}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<div className="text-green-500">
-			<h1>Hello Hassan</h1>
-		</div>
-	);
-}

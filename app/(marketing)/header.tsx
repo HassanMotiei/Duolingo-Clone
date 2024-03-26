242e4ff419ef9e3db736423aea89377725abdffe
Hassan Motiei <123471966+HassanMotiei@users.noreply.github.com>
2024-03-26 18:38:13 +0330

    Add learn Route and Sidebar Components

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -10,6 +10,8 @@ import {
 import { Loader } from "lucide-react";
 import Image from "next/image";
 
+import { ThemeToggle } from "@/components/themeToggle";
+
 export const Header = () => {
 	return (
 		<header className="h-20 w-full border-b-2 border-slate-200 px-4">
@@ -25,25 +27,28 @@ export const Header = () => {
 						Duolingo
 					</h1>
 				</div>
-				<ClerkLoading>
-					<Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-				</ClerkLoading>
-				<ClerkLoaded>
-					<SignedIn>
-						<UserButton afterSignOutUrl="/"/>
-					</SignedIn>
-					<SignedOut>
-						<SignInButton
-							mode="modal"
-							afterSignInUrl="/learn"
-							afterSignUpUrl="/learn"
-						>
-							<Button size={"lg"} variant={"ghost"}>
-								Login
-							</Button>
-						</SignInButton>
-					</SignedOut>
-				</ClerkLoaded>
+				<div className="flex items-center gap-5">
+					<ThemeToggle />
+					<ClerkLoading>
+						<Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<SignedIn>
+							<UserButton afterSignOutUrl="/" />
+						</SignedIn>
+						<SignedOut>
+							<SignInButton
+								mode="modal"
+								afterSignInUrl="/learn"
+								afterSignUpUrl="/learn"
+							>
+								<Button size={"lg"} variant={"ghost"}>
+									Login
+								</Button>
+							</SignInButton>
+						</SignedOut>
+					</ClerkLoaded>
+				</div>
 			</div>
 		</header>
 	);
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Nunito({ subsets: ["latin"] });
@@ -9,6 +10,9 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Duolingo",
 	description: "The world's best way to learn a language",
+	icons: {
+		icon: "/mascot.svg",
+	},
 };
 
 export default function RootLayout({
@@ -17,10 +21,22 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider>
-			<html lang="en">
-				<body className={font.className}>{children}</body>
-			</html>
-		</ClerkProvider>
+		<>
+			<ClerkProvider>
+				<html lang="en" suppressHydrationWarning>
+					<body className={font.className}>
+						<ThemeProvider
+							attribute="class"
+							// defaultTheme="system"
+							defaultTheme="light"
+							enableSystem
+							disableTransitionOnChange
+						>
+							{children}
+						</ThemeProvider>
+					</body>
+				</html>
+			</ClerkProvider>
+		</>
 	);
 }
